2f593368ca6730f8d28c336abf6ae5d77873601e
Ubuntu <azureuser@postgresql-test2.3kj2zpsc2z0utjzroreexx1u0h.rx.internal.cloudapp.net>
2021-02-25 23:51:53 +0000

    cleaning requirements.txt

--- client/install_packages.sh
+++ client/install_packages.sh
@@ -7,10 +7,10 @@ sudo ln -s /usr/bin/python3.6 /usr/bin/python
 # Install Ubuntu packages
 
 echo -e "\n--- Installing Ubuntu packages ---\n"
-apt-get -qq update
-apt-get -y install python3-pip python-dev python-mysqldb rabbitmq-server gradle default-jdk libmysqlclient-dev python3-tk
+sudo apt-get -qq update
+sudo apt-get -y install python3-pip python-dev python-mysqldb rabbitmq-server gradle default-jdk libmysqlclient-dev python3-tk
 
 echo -e "\n--- Installing Python packages ---\n"
-pip3 install --upgrade pip
-pip install -r ${REPOPATH}/client/requirements.txt
+sudo pip3 install --upgrade pip
+sudo pip install -r ${REPOPATH}/client/requirements.txt
 

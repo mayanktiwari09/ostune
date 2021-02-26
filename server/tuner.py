b340de80b0bce6063067791b0e4cec7c1d5b2196
Mayank Tiwary <mayanktiwari09@gmail.com>
2021-02-25 21:14:41 -0800

    serializing metrics

--- server/tuner.py
+++ server/tuner.py
@@ -4,6 +4,7 @@ from flask import request, jsonify
 from knobs import Knobs
 from metrics import Metrics
 from constants import *
+
 app = flask.Flask(__name__)
 
 @app.route('/api/v1/get_recommendation', methods=['POST'])
@@ -14,7 +15,8 @@ def json_example():
     knobs = Knobs(vmSwapiness=knobsJson[VM_SWAPINESS])
     metrics = Metrics(throughput=metricsJson[THROUGHPUT])
 
+
     recommendedKnobs = Knobs(vmSwapiness=knobs.vmSwapiness)
     return jsonify(recommendedKnobs.serialize())
 
-app.run(debug=True, port=8000)
+app.run(debug=True, port=8000, host='0.0.0.0')
